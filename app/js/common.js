b3c95a7fe97c904b1ad2cf0369a9760b70dec280
rudolif <rudolifrudolif@gmail.com>
2019-04-18 23:35:10 +0300

    two page start commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,31 @@
 $(function() {
 
+
+
+//-------------------------------анімація цифр---------------------------------------
+
+if($('*').is('#about')) {
+  var show = true;
+  var countbox = ".about";
+  $(window).on("scroll load resize", function () {
+      if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+      var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+      var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+      var w_height = $(window).height(); // Высота окна браузера
+      var d_height = $(document).height(); // Высота всего документа
+      var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+      if (w_top + 810 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+          $('.about__list_item h3').spincrement({
+              thousandSeparator: "",
+              duration: 6000
+          });
+          show = false;
+      }
+  });
+}
+
+
+
 //-----------------------------training-slider-----------------------------
   $('.training__slider').slick({
     centerMode: true,
@@ -102,33 +128,14 @@ $(function() {
       $('body,html').animate({scrollTop: top - 60}, 'slow', 'swing');
   });
 
-    //-------------------------------анімація цифр---------------------------------------
-      var show = true;
-      var countbox = ".about";
-      $(window).on("scroll load resize", function () {
-          if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-          var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-          var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-          var w_height = $(window).height(); // Высота окна браузера
-          var d_height = $(document).height(); // Высота всего документа
-          var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-          if (w_top + 810 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-              $('.about__list_item h3').spincrement({
-                  thousandSeparator: "",
-                  duration: 6000
-              });
-              show = false;
-          }
-      });
-
   
 });
 
 //----------------------------------------preloader----------------------------------
 
-  // $(window).on('load', function(){
-  //   $('.preloader').delay(2500).fadeOut('slow');
-  // });
+  $(window).on('load', function(){
+    $('.preloader').delay(2500).fadeOut('slow');
+  });
 
 //--------------------------------------js----------------------------------------
 
